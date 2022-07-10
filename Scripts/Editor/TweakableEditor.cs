77b559d28a26ef37cff2f6363596dd00a3967662
Kiwi <17229588+KiwiGameDev@users.noreply.github.com>
2022-07-10 23:29:07 +0800

    Added TweakableEditor.cs

---
+++ Scripts/Editor/TweakableEditor.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Kiwi.Editor
+{
+	public abstract class TweakableEditor : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+         
+			OnBeforeDefaultInspector();
+			DrawPropertiesExcluding(serializedObject, GetExcludedPropertiesInDefaultInspector());
+			OnAfterDefaultInspector();
+ 
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		protected abstract void OnBeforeDefaultInspector();
+		protected abstract void OnAfterDefaultInspector();
+		protected virtual string[] GetExcludedPropertiesInDefaultInspector() => emptyStringArray;
+
+		static readonly string[] emptyStringArray = Array.Empty<string>();
+	}
+}
