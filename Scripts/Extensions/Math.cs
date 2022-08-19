eb5a8297af06d7f4c7e49ba16859467d46998c60
Bruce <17229588+KiwiGameDev@users.noreply.github.com>
2022-08-19 20:54:50 +0800

    Added Math Map method

---
+++ Scripts/Extensions/Math.cs
@@ -0,0 +1,20 @@
+namespace Kiwi.Extensions
+{
+	public static class Math
+	{
+		public static double Map(double value, double x1, double y1, double x2, double y2)
+		{
+			return (value - x1) / (y1 - x1) * (y2 - x2) + x2;
+		}
+		
+		public static float Map(float value, float x1, float y1, float x2, float y2)
+		{
+			return (value - x1) / (y1 - x1) * (y2 - x2) + x2;
+		}
+		
+		public static int Map(int value, int x1, int y1, int x2, int y2)
+		{
+			return (value - x1) / (y1 - x1) * (y2 - x2) + x2;
+		}
+	}
+}
